b267803294629b4a9b78ed7ea39929c762ca6818
allen <rocha021@rangers.uwp.edu>
2019-11-27 19:20:54 -0600

    Refactored

---
+++ Pi/Outside/BME680.py
@@ -0,0 +1,9 @@
+import board
+import busio
+import adafruit_bme680
+
+
+def read() -> (float, float, int, float):
+    i2c = busio.I2C(board.SCL, board.SDA)
+    sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+    return sensor.temperature, sensor.humidity, sensor.gas, sensor.pressure
--- Pi/Plant/STEMMA.py
+++ Pi/Plant/STEMMA.py
@@ -4,9 +4,10 @@ import busio
 from adafruit_seesaw.seesaw import Seesaw
 
 
-def read() -> float:
+def read() -> (float, float):
     i2c_bus = busio.I2C(SCL, SDA)
     ss = Seesaw(i2c_bus, addr=0x36)
     # read temperature from the temperature sensor
-    return ss.get_temp()
+    # read moisture level through capacitive touch pad
+    return ss.get_temp(), ss.moisture_read()
 
